9d672292eaa0cdf20cc2dbf292fb9503d1117029
gonzart <45474363+gonzart@users.noreply.github.com>
2022-07-27 18:28:32 -0500

    Add files via upload

---
+++ CalcPresSensDiscXYv1DAFuenteCilindroLBMat.m
@@ -0,0 +1,191 @@
+function [ t,P ] = CalcPresSensDiscXYv1DAFuenteCilindroLBMat(R,nPixS,nPixF,distCC,FR,Alpha,ZDisc,LongCil,RebZ,deltat,tau,c)
+% ---------Argumentos de entrada---------
+% R : Radio del sensor
+% nPixS : número de arcos que discretizarán al sensor
+% nPixF : número de arcos que discretizarán a la muestra
+
+% -----------Generando el disco de fuente discretizado con arcos-----------
+if FR<distCC
+    RadioDeteccion=linspace(distCC-FR,distCC+FR,nPixF)';
+else
+    RadioDeteccion=linspace(FR/nPixF,distCC+FR,nPixF)';
+%     ThetaInDegreesF = atan2d(norm(cross([0,0,0],[Fcen(1),Fcen(2),0])),dot([0,0,0],[Fcen(1),Fcen(2),0]));
+end
+% Pf=[cosd(ThetaInDegreesF).*discreteRF,sind(ThetaInDegreesF).*discreteRF,Fcen(3).*ones(size(discreteRF))];
+Pf=zeros(2*nPixF,3);
+ArcoF=zeros(nPixF,1);
+
+% ------------------Sección cilindro-------------------------------------
+cilindro=linspace(ZDisc-LongCil,ZDisc,RebZ)';
+ILB=exp(Alpha.*(cilindro-ZDisc));
+% ----------------------------------------------------------------
+
+% Se genera el disco más externo-------------------
+for i=1:nPixF
+    [xoutF,youtF] = circcirc(0,0,RadioDeteccion(i),distCC,0,FR);
+    if isnan(xoutF(1))
+        Pf(i,:)=[distCC+RadioDeteccion(i),0,ZDisc];
+        ArcoF(i)=2*pi*RadioDeteccion(i);
+    else
+        Pf(i,:)=[xoutF(1),youtF(1),ZDisc];
+        %--arco fuente
+%         x=(FR*FR-RadioDeteccion(i)*RadioDeteccion(i)+distCC*distCC)/(2*RadioDeteccion(i)*distCC);
+        x=(RadioDeteccion(i)*RadioDeteccion(i)+distCC*distCC-FR*FR)/(2*RadioDeteccion(i)*distCC);
+        alf=real(acos(x));%es un numero complejo por la precision numerica de las cotas [-1 1]
+        ArcoF(i)=2*RadioDeteccion(i)*alf;
+    end
+end
+Pf((nPixF+1:2*nPixF),:)=[Pf(1:nPixF,1),Pf(1:nPixF,1),(ZDisc-LongCil).*ones(nPixF,1)];
+
+%-----Calculo de la ventana de tiempo, Tiempo de vuelo mínimo y máximo-----
+%-----del disco más externo-----
+% 1° se desplazan las coordenadas de los elemento diferenciales de la muestra al
+% eje X
+Pf(:,1)=sqrt(dot(Pf(:,1:2),Pf(:,1:2),2));
+Pf(:,2)=0;
+
+% -----------------------sección ventanas ajustadas------------------------
+
+% Calculamos la distancia mínima de las particulas que estan contenidas en
+% el plano del sensor y obtenemos la mínima y máxima distancia
+Pfdist=Pf(:,1)<=R;
+PfdistI=Pf(Pfdist,:);
+MinI=min(PfdistI(:,3));
+% Cálculo valor máximo
+PfdistINMax=[PfdistI(:,1)+R,PfdistI(:,2),PfdistI(:,3)];
+PfdistINMax=sqrt(dot(PfdistINMax,PfdistINMax,2));
+MaxI=max(PfdistINMax);
+
+% Calculamos la distancia mínima de las particulas que no estan contenidas
+% el plano del sensor y obtenemos la mínima y máxima distancia
+Pfdist=Pf(:,1)>R;
+PfdistE=Pf(Pfdist,:);
+% Cálculo valor mínimo
+PfdistENMin=[PfdistE(:,1)-R,PfdistE(:,2),PfdistE(:,3)];
+PfdistENMin=sqrt(dot(PfdistENMin,PfdistENMin,2));
+MinE=min(PfdistENMin);
+% Calculo valor máximo
+PfdistENMax=[PfdistE(:,1)+R,PfdistE(:,2),PfdistE(:,3)];
+PfdistENMax=sqrt(dot(PfdistENMax,PfdistENMax,2));
+MaxE=max(PfdistENMax);
+% calculamos los valores de tiempo mínimo y máximo
+Ww=3;
+if MinI>MinE
+    if isempty(MinE)
+        tmin=(MinI/c)-(Ww*tau);
+    else
+        tmin=(MinE/c)-(Ww*tau);
+    end
+else
+    if isempty(MinI)
+        tmin=(MinE/c)-(Ww*tau);
+    else
+        tmin=(MinI/c)-(Ww*tau);
+    end
+end
+if MaxI>MaxE
+    if isempty(MaxI)
+        tmax=(MaxE/c)+(Ww*tau);
+    else
+        tmax=(MaxI/c)+(Ww*tau);
+    end
+else
+    if isempty(MaxE)
+        tmax=(MaxI/c)+(Ww*tau);
+    else
+        tmax=(MaxE/c)+(Ww*tau);
+    end
+end
+
+clear Pfdist PfdistE PfdistI PfdistENMin PfdistENMax PfdistINMax
+% ----------------------------------------------------------------
+
+
+t=tmin:deltat:tmax;
+% P=zeros(size(t));
+P=zeros(RebZ,size(t,2));
+
+width=floor(2*tau/deltat);      %ancho en pixeles que genera la ventana
+posmin=floor(tmin/deltat);     %indice que ocuparía el tiempo de vuelo 
+%                                minimo en un vector completo
+
+a=-4/(c^2*tau^2);
+
+for d=1:RebZ
+    for e=1:nPixF;
+
+        if Pf(e,1)>R
+    %         rVec=Pf(e,1)-R:R/nPix:Pf(e,1)+R;
+            rVec=linspace(Pf(e,1)-R,Pf(e,1)+R,nPixS);
+        else
+    %         rVec=R/nPix:R/nPix:Pf(e,1)+R;% no puede comenzar en cero
+            rVec=linspace(R/nPixS,(Pf(e,1)+R),nPixS);
+        end
+        for i=1:nPixS
+
+            [xout,yout] = circcirc(0,0,R,Pf(e,1),0,rVec(i));    
+
+            if isnan(xout(1))
+                S=[Pf(e,1),rVec(i),0];%%%%%cambiar
+                Vd=([Pf(e,1),Pf(e,2),cilindro(d)]-S);%vector distancia resultante de combinar elementos sensor-fuente
+                Area=pi*rVec(i)*rVec(i);
+            else
+                S=[xout(1),yout(1),0];
+                Vd=([Pf(e,1),Pf(e,2),cilindro(d)]-S);%vector distancia resultante de combinar elementos sensor-fuente
+                %--------------Calculo diferencial de área----------------
+    %             x=(rVec(i)*rVec(i)-R*R+Pf(e,1)*Pf(e,1))/(2*rVec(i)*Pf(e,1));
+                x=((rVec(i)*rVec(i))-(R*R)+(Pf(e,1)*Pf(e,1)))/(2*(rVec(i))*Pf(e,1));
+                alf=real(acos(x));%es un numero complejo por la precision numerica de las cotas [-1 1]
+                PP=(dot([Pf(e,1),0],[xout(1),yout(1)],2)/(Pf(e,1)*R));
+                Theta=real(acos(PP));
+                TS=(abs(xout(1)))*(abs(yout(1)));
+                if Theta>pi/2
+                    PO=alf*rVec(i)*rVec(i); 
+                    TO=rVec(i)*(cos(alf))*(abs(yout(1)));
+                    LO=PO-TO;
+                    PS=(pi-Theta)*R*R;
+    %                 TS=(abs(xout(1)))*(abs(yout(1)));%sacar del if
+                    LS=PS-TS;
+                    Area=(pi*R*R)+LO-LS;
+                else % Theta<=pi/2
+                    PS=(Theta)*R*R;
+    %                 TS=(abs(xout(1)))*(abs(yout(1)));
+                    if alf<pi/2
+                        PO=alf*rVec(i)*rVec(i);
+                        TO=rVec(i)*(cos(alf))*(abs(yout(1)));
+                        LO=PO-TO;
+                        LS=PS-TS;
+                        Area=LO+LS;
+                    else % alf>=pi/2
+                        PO=(pi-alf)*rVec(i)*rVec(i);
+                        TO=rVec(i)*(cos(pi-alf))*(abs(yout(1)));
+                        LO=PO-TO;
+                        LS=PS-TS;
+                        Area=(rVec(i)*rVec(i)*pi)+LS-LO;
+                    end
+                end
+
+                %-------------------------------------------------------------
+            end
+
+            if i==1
+                DA=Area;
+            else
+                DA=Area-AreaPast;
+            end
+
+            Dist=sqrt(dot(Vd,Vd,2));%vector distancia
+            pii=floor((Dist./c)./deltat);
+            pf=pii+width-posmin;
+            pii=pii-width-posmin;
+            tiempos=t(pii:pf);
+            Coef = c.*tiempos-Dist;
+            Pres=-(Coef./Dist).*exp(a.*Coef.^2).*DA.*ArcoF(e).*ILB(d);
+            P(d,pii:pf)=P(d,pii:pf)+Pres;
+            AreaPast=Area;
+        end
+    end
+%     P=P.*ILB(d);
+end
+
+end
---
+++ PruebaLambertBeerMuestraCilindroFuenteArcos.m
@@ -0,0 +1,122 @@
+close all
+clear variables
+clc
+
+tau=10*(10^-9);         %Tau
+deltat=500*(10^-12);    %periodo de muestreo
+c=1500;                 %velocidad del sonido
+ROl=0.0015875;
+NpS=400;
+NpF=150;
+distCC=0;
+RF=0.003/2;
+
+
+%-------------------------- sección alpha 1300-------------------------
+LongCil=3*(10^-3);
+RebZ=ceil((LongCil*11)/(60*(10^-6)));
+Alpha=1300;
+
+ZDisc=0.045;
+tic;
+[ t1,P1 ] = CalcPresSensDiscXYv1DAFuenteCilindroLBMat(ROl,NpS,NpF,distCC,RF,Alpha,ZDisc,LongCil,RebZ,deltat,tau,c);
+tC1=toc;
+filename = 'Pt1.mat';
+save(filename,'t1','P1')
+
+clear t1 P1
+
+
+ZDisc=0.04;
+tic;
+[ t2,P2 ] = CalcPresSensDiscXYv1DAFuenteCilindroLBMat(ROl,NpS,NpF,distCC,RF,Alpha,ZDisc,LongCil,RebZ,deltat,tau,c);
+tC2=toc;
+filename = 'Pt2.mat';
+save(filename,'t2','P2')
+clear t2 P2
+
+
+ZDisc=0.035;
+tic;
+[ t3,P3 ] = CalcPresSensDiscXYv1DAFuenteCilindroLBMat(ROl,NpS,NpF,distCC,RF,Alpha,ZDisc,LongCil,RebZ,deltat,tau,c);
+tC3=toc;
+filename = 'Pt3.mat';
+save(filename,'t3','P3')
+clear t3 P3
+
+ZDisc=0.03;
+tic;
+[ t4,P4 ] = CalcPresSensDiscXYv1DAFuenteCilindroLBMat(ROl,NpS,NpF,distCC,RF,Alpha,ZDisc,LongCil,RebZ,deltat,tau,c);
+tC4=toc;
+filename = 'Pt4.mat';
+save(filename,'t4','P4')
+clear t4 P4
+
+%-------------------------- sección alpha 25500-------------------------
+% LongCil=3*(10^-3);
+LongCil=0.00015;
+RebZ=ceil((LongCil*11)/(60*(10^-6)));
+Alpha=25500;
+
+ZDisc=0.045;
+tic;
+[ t5,P5 ] = CalcPresSensDiscXYv1DAFuenteCilindroLBMat(ROl,NpS,NpF,distCC,RF,Alpha,ZDisc,LongCil,RebZ,deltat,tau,c);
+tC5=toc;
+filename = 'Pt5.mat';
+save(filename,'t5','P5')
+clear t5 P5
+
+
+ZDisc=0.04;
+tic;
+[ t6,P6 ] = CalcPresSensDiscXYv1DAFuenteCilindroLBMat(ROl,NpS,NpF,distCC,RF,Alpha,ZDisc,LongCil,RebZ,deltat,tau,c);
+tC6=toc;
+filename = 'Pt6.mat';
+save(filename,'t6','P6')
+clear t6 P6
+
+
+ZDisc=0.035;
+tic;
+[ t7,P7 ] = CalcPresSensDiscXYv1DAFuenteCilindroLBMat(ROl,NpS,NpF,distCC,RF,Alpha,ZDisc,LongCil,RebZ,deltat,tau,c);
+tC7=toc;
+filename = 'Pt7.mat';
+save(filename,'t7','P7')
+clear t7 P7
+
+ZDisc=0.03;
+tic;
+[ t8,P8 ] = CalcPresSensDiscXYv1DAFuenteCilindroLBMat(ROl,NpS,NpF,distCC,RF,Alpha,ZDisc,LongCil,RebZ,deltat,tau,c);
+tC8=toc;
+filename = 'Pt8.mat';
+save(filename,'t8','P8')
+clear t8 P8
+
+filename = 'WorkS.mat';
+save(filename)
+
+% %--------------------------------------------------------------------
+% 
+% c=1500;                 %velocidad del sonido
+% d=(3*(.1^2))^(1/2);     %Distancia máxima
+% % d=0.4;
+% % d=0.25;
+% deltat=500*(10^-12);    %periodo de muestreo
+% n=d/c;  
+% t=0:deltat:n-deltat;    %vector tiempo
+% 
+% % función anónima, -------------------------------------------------------
+% %Respuesta al impulso del sensor usado
+% Olympus=@(x)(...
+%     (0.1 *exp(-0.5*(((x-16.5e-9)./0.9e-9).^2)))+(-0.5 *exp(-0.5*(((x-19e-9)./0.9e-9).^2)))+ ...
+%     (0.6 *exp(-0.5*(((x-22e-9)./0.9e-9).^2)))+(-0.3 *exp(-0.5*(((x-25e-9)./0.9e-9).^2)))+...
+%     (0.17*exp(-0.5*(((x-28e-9)./0.9e-9).^2)))+(-0.13*exp(-0.5*(((x-31e-9)./0.9e-9).^2)))+...
+%     (0.05*exp(-0.5*(((x-34e-9)./0.9e-9).^2))));
+% %calculando ek kernel con la respuesta al impulso
+% % tk=0:1e-9:50e-9;
+% tk=t(1:100);
+% RIO=Olympus(tk);
+% 
+% VPuntualRIO=conv(P2,RIO,'same');
+% figure();
+% plot(ti2,VPuntualRIO)
